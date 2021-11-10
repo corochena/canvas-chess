219c6aff8aed59ca822a8c653f0ff56e4c88e3cc
corochena <corochena@gmail.com>
2021-11-10 11:22:59 -0600

    Feat - Draw chess board

--- script.js
+++ script.js
@@ -4,4 +4,22 @@ const ctx = canvas.getContext('2d');
 const img = new Image();
 img.src = 'https://i.ibb.co/kSrfw6R/Chess-Pieces-Sprite-compressed.png';
 
-ctx.drawImage(img,0,0)
+const boardLen = 600;
+const sqLen = boardLen / 8;
+
+const darkCol = '#77995b';
+const lightCol = '#eeeeee';
+
+function drawBoard() {
+  let i = 0;
+
+  for (let row = 0; row < 8; row++) {
+    for (let col = 0; col < 8; col++) {
+      let color = (row + col) % 2 != 0 ? darkCol : lightCol;
+      ctx.fillStyle = color;
+      ctx.fillRect(row * sqLen, col * sqLen, sqLen, sqLen);
+    }
+  }
+}
+
+drawBoard()
