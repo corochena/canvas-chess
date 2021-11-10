64422a6c3afdb3d9b753bb9518a09d2e0be176d3
corochena <corochena@gmail.com>
2021-11-10 11:18:31 -0600

    1 file modified

---
+++ script.js
@@ -0,0 +1,5 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+const img = new Image();
+img.src = 'https://i.ibb.co/kSrfw6R/Chess-Pieces-Sprite-compressed.png';
